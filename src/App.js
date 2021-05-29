2b5e628e7d9dab97bef46df003cc6ff4997ba51f
aami-12 <iamaamir253@gmail.com>
2021-05-29 11:15:31 +0530

    data-grid

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Datatable from './component/Datatable';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Datatable />
+    </div>
+  );
+}
+
+export default App;
